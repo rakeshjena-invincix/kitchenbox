e400ef95b9c6cf992e9bc542745a306df65a75cf
rakeshjena_invincix <rakesh.jena@invincix.com>
2022-09-05 12:40:34 +0530

    changed

---
+++ src/pages/Subscription/SubscriptionPlan.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+import "./../Home/Home.css";
+
+const SubscriptionPlan = () => {
+  return <></>;
+};
+
+export default SubscriptionPlan;
