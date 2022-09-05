2258906ed8586ade46ec77163affa7b69f312c87
rajeeb-cavemendev <95476849+rajeeb-cavemendev@users.noreply.github.com>
2022-09-05 11:48:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Billing from "./pages/Billing/Billing";
 import HomePage from "./pages/Home/HomePage";
+import Subscription from "./pages/Subscription/Subscription";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/billing" element={<Billing />} />
+        <Route path="/subscription" element={<Subscription />} />
       </Routes>
     </>
   );
